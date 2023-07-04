45d7b7f8632e28f229912f00eec39f211e1e8115
duyhiep523 <hiep06112003@gmail.com>
2023-07-04 13:23:10 +0700

    làm lại

--- 03-07-2023/app.js
+++ 03-07-2023/app.js
@@ -1,44 +1,41 @@
-var txt_user = document.querySelector("form #user");
-var txt_pass = document.querySelector("form #pass");
-var errrrr = document.getElementById("er");
-var in4 = document.getElementById("name_us");
-var txt_error = "";
-var hiih;
-document.getElementById("btnSubmit").addEventListener("click", myfunction);
-function myfunction(event) {
+var user = document.getElementById("user");
+var pass = document.getElementById("pass");
+var err = document.getElementById("err");
+var form = document.getElementById("for")
+form.addEventListener("submit", function (event) {
     event.preventDefault();
-    if (txt_pass.value == "" || txt_user.value == "") {
-        txt_error += "Vui lòng nhập đầy đủ thông tin"
-        write_error(txt_error);
+    if (user.value == "") {
+        count("Vui lòng nhập đầy đủ thông tin")
+        user.focus();
+        return false;
+    }
+    if (pass.value == "") {
+        count("Vui lòng nhập đầy đủ thông tin")
+        pass.focus();
+        return false;
+    }
+    if (user.value != "admin") {
+        count("Tài khoản không tồn tại")
+        user.focus()
+        return false;
 
-        txt_error = ""
+    } else if (pass.value != "admin") {
+        count("Mật khẩu không chính xác")
+        pass.focus()
         return false;
+    } else {
+        sessionStorage.setItem("name","Nguyễn Duy Hiệp")
+        window.location = "index.html"
+        return true;
     }
-    if (txt_user.value != "admin") {
-        txt_error += "Tài khoản không tồn tại"
-        write_error(txt_error);
-        txt_error = ""
-        return false;
-    } else if (txt_pass.value != "admin") {
-        txt_error += "Mật khẩu sai"
-        write_error(txt_error);
-        txt_error = ""
-        return false;
-    }
-    else {
-        hiih = txt_user.value;
-        window.location.href = "index.html";
-        in4.textContent = hiih;
-    }
-}
 
-function write_error(sos) {
-    errrrr.style.display = "flex"
-    errrrr.textContent = sos;
+})
+function count(sos) {
+    err.style.display = "flex"
+    err.textContent = sos;
     setTimeout(function () {
-        errrrr.textContent = "";
-        errrrr.style.display = "none"
-    }, 3000);
+        err.textContent = "";
+        err.style.display = "none"
+    }, 3000)
+
 }
-
-
