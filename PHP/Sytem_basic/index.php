21f971089ec35eca224aa3d7f01cd475e25ab05f
duyhiep523 <hiep06112003@gmail.com>
2023-07-26 14:13:38 +0700

    dieu huong co ban

---
+++ PHP/Sytem_basic/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'inc/header.php';
+?>
+<main>
+    <div class="container-fluid ">
+        <p class="text-center bg-success" height="500">content</p>
+    </div>
+</main>
+<?php
+
+// require 'inc/footer.php';
+?>
